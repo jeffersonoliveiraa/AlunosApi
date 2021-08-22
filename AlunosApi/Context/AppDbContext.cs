81b810b91766a57f9fe0eec92973ed688e722ac7
jeffersonoliveiraa <jeffersonoliveirarodrigues75@gmail.com>
2021-08-22 11:42:17 -0300

    Add project files.

---
+++ AlunosApi/Context/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using AlunosApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlunosApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Aluno>().HasData(
+                new Aluno
+                {
+                    Id = 1,
+                    Nome = "Jefferson Oliveira Rodrigues",
+                    Email = "jefferson.oliveira@gmail.com",
+                    Idade = 23
+                },
+                new Aluno
+                {
+                    Id = 2,
+                    Nome = "Rebeca Estevão Alexandre",
+                    Email = "rebecaestevao@gmail.com",
+                    Idade = 22
+                }
+            );
+
+        }
+    }
+}
+
---
+++ AlunosApi/Migrations/20210822142634_PopulaTabela.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AlunosApi.Migrations
+{
+    public partial class PopulaTabela : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 1, "jefferson.oliveira@gmail.com", 23, "Jefferson Oliveira Rodrigues" });
+
+            migrationBuilder.InsertData(
+                table: "Alunos",
+                columns: new[] { "Id", "Email", "Idade", "Nome" },
+                values: new object[] { 2, "rebecaestevao@gmail.com", 22, "Rebeca Estevão Alexandre" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Alunos",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
